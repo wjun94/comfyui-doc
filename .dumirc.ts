8a0c4cd66c87c608228b631999a51efa19ac1740
wjun94 <kasepa1994@gmail.com>
2026-01-14 22:44:34 +0800

    feat: 解决gitpages路径问题

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  title: '我的dumi文档',
+  base: '/comfyui-doc/',
+  publicPath: '/comfyui-doc/', // 与 base 保持一致
+
+  // 关键配置：部署到 GitHub Pages 的基础路径
+  // 情况1：部署到 个人/组织主页（仓库名必须是 [用户名].github.io）
+  // base: '/',
+  // publicPath: '/',
+
+  outputPath: 'dist', // Dumi 打包输出目录（默认就是 dist，可显式指定）
+
   themeConfig: {
     name: 'comfyui文档',
   },
